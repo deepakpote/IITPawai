7e8891ff4a8873400fb4428baa2c441b47041b1d
Dipak Pote <deepakpote.007@gmail.com>
2016-12-09 19:04:32 +0530

    Removed unused/commented code for District serializer & return success code

--- endPoints/commons/views.py
+++ endPoints/commons/views.py
@@ -4,17 +4,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import list_route
 
-from commons.models import code , news #district,
-from commons.serializers import codeSerializer , newsSerializer #districtSerializer
+from commons.models import code , news 
+from commons.serializers import codeSerializer , newsSerializer 
+from mitraEndPoints import constants
  
-#  
-# class DistrictViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = district.objects.all().order_by('districtName')
-#     serializer_class = districtSerializer
-
+ 
 class CodeViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -25,17 +19,17 @@ class CodeViewSet(viewsets.ModelViewSet):
     def list(self, request):
         queryset = code.objects.all()
         serializer = codeSerializer(queryset, many = True)
-        return Response({"response_message": "success", "data": serializer.data})
+        return Response({"response_message": constants.messages.success, "data": serializer.data})
     
 class NewsViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to read the news
     """
-    queryset = news.objects.all().order_by('createdOn')
+    queryset = news.objects.all().order_by('-createdOn')
     serializer_class = newsSerializer
     
     def list(self, request):
-        queryset = news.objects.all()
+        queryset = news.objects.all().order_by('-createdOn')
         serializer = newsSerializer(queryset, many = True)
-        return Response({"response_message": "success", "data": serializer.data})
+        return Response({"response_message": constants.messages.success, "data": serializer.data})
     
