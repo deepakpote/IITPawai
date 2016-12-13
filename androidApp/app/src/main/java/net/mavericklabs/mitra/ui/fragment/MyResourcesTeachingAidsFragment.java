038cf4baed7d04f21af6e8afa9f896dbba81d443
Vishakha Damle <vishakha@Vishakhas-MacBook-Pro.local>
2016-12-13 15:00:01 +0530

    temporarily hide filters from my saved resources

--- androidApp/app/src/main/java/net/mavericklabs/mitra/ui/fragment/MyResourcesTeachingAidsFragment.java
+++ androidApp/app/src/main/java/net/mavericklabs/mitra/ui/fragment/MyResourcesTeachingAidsFragment.java
@@ -69,14 +69,14 @@ import retrofit2.Response;
 
 
 public class MyResourcesTeachingAidsFragment extends Fragment {
-    @BindView(R.id.subject_spinner)
-    Spinner subjectSpinner;
-
-    @BindView(R.id.grade_spinner)
-    Spinner gradeSpinner;
-
-    @BindView(R.id.type_spinner)
-    Spinner typeSpinner;
+//    @BindView(R.id.subject_spinner)
+//    Spinner subjectSpinner;
+//
+//    @BindView(R.id.grade_spinner)
+//    Spinner gradeSpinner;
+//
+//    @BindView(R.id.type_spinner)
+//    Spinner typeSpinner;
 
     @BindView(R.id.content_recycler_view)
     RecyclerView contentRecyclerView;
@@ -119,88 +119,90 @@ public class MyResourcesTeachingAidsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_my_resources_teaching_aids, container, false);
         ButterKnife.bind(this, rootView);
 
-        final List<CommonCode> subjects = new ArrayList<>(CommonCodeUtils.getSubjects());
-        final List<CommonCode> grades = new ArrayList<>(CommonCodeUtils.getGrades());
-        final List<CommonCode> types = new ArrayList<>(CommonCodeUtils.getFileTypes());
-
-        //Header - not a valid value
-        subjects.add(0, new CommonCode("", "","Subject", "Subject", 0));
-        grades.add(0,new CommonCode("","","Grade","Grade",0));
-        types.add(0,new CommonCode("","","Type","Type",0));
-
         fragment = (MyResourcesFragment) getParentFragment();
 
-
-
-        SpinnerArrayAdapter adapter = new SpinnerArrayAdapter(getActivity(), R.layout.custom_spinner_item_header,
-                subjects);
-        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
-        subjectSpinner.setAdapter(adapter);
-        subjectSpinner.setSelection(0 ,false);
-
-        RealmResults<DbUser> dbUser = Realm.getDefaultInstance()
-                .where(DbUser.class).findAll();
-        if(dbUser.size() == 1) {
-            DbUser user = dbUser.get(0);
-            language = user.getPreferredLanguage();
-            Logger.d(" language " + language);
-        }
-
-
-        subjectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                CommonCode grade = (CommonCode) gradeSpinner.getSelectedItem();
-                CommonCode fileType = (CommonCode) typeSpinner.getSelectedItem();
-                loadMyTeachingAids(fileType.getCodeID(), language, subjects.get(i).getCodeID(), grade.getCodeID());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        SpinnerArrayAdapter gradeAdapter = new SpinnerArrayAdapter(getActivity(), R.layout.custom_spinner_item_header,
-                grades);
-        gradeAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
-        gradeSpinner.setAdapter(gradeAdapter);
-        gradeSpinner.setSelection(0 ,false);
-
-        gradeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                CommonCode subject = (CommonCode) subjectSpinner.getSelectedItem();
-                CommonCode fileType = (CommonCode) typeSpinner.getSelectedItem();
-                loadMyTeachingAids(fileType.getCodeID(), language, grades.get(i).getCodeID() , subject.getCodeID());
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        SpinnerArrayAdapter typeAdapter = new SpinnerArrayAdapter(getActivity(), R.layout.custom_spinner_item_header,
-                types);
-        typeAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
-        typeSpinner.setAdapter(typeAdapter);
-        typeSpinner.setSelection(0 ,false);
-
-        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                CommonCode subject = (CommonCode) subjectSpinner.getSelectedItem();
-                CommonCode grade = (CommonCode) gradeSpinner.getSelectedItem();
-                loadMyTeachingAids(types.get(i).getCodeID(), language, grade.getCodeID() , subject.getCodeID());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+//        final List<CommonCode> subjects = new ArrayList<>(CommonCodeUtils.getSubjects());
+//        final List<CommonCode> grades = new ArrayList<>(CommonCodeUtils.getGrades());
+//        final List<CommonCode> types = new ArrayList<>(CommonCodeUtils.getFileTypes());
+//
+//        //Header - not a valid value
+//        subjects.add(0, new CommonCode("", "","Subject", "Subject", 0));
+//        grades.add(0,new CommonCode("","","Grade","Grade",0));
+//        types.add(0,new CommonCode("","","Type","Type",0));
+//
+//
+//
+//
+//
+//        SpinnerArrayAdapter adapter = new SpinnerArrayAdapter(getActivity(), R.layout.custom_spinner_item_header,
+//                subjects);
+//        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+//        subjectSpinner.setAdapter(adapter);
+//        subjectSpinner.setSelection(0 ,false);
+//
+//        RealmResults<DbUser> dbUser = Realm.getDefaultInstance()
+//                .where(DbUser.class).findAll();
+//        if(dbUser.size() == 1) {
+//            DbUser user = dbUser.get(0);
+//            language = user.getPreferredLanguage();
+//            Logger.d(" language " + language);
+//        }
+//
+//
+//        subjectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                CommonCode grade = (CommonCode) gradeSpinner.getSelectedItem();
+//                CommonCode fileType = (CommonCode) typeSpinner.getSelectedItem();
+//                loadMyTeachingAids(fileType.getCodeID(), language, subjects.get(i).getCodeID(), grade.getCodeID());
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
+//
+//        SpinnerArrayAdapter gradeAdapter = new SpinnerArrayAdapter(getActivity(), R.layout.custom_spinner_item_header,
+//                grades);
+//        gradeAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+//        gradeSpinner.setAdapter(gradeAdapter);
+//        gradeSpinner.setSelection(0 ,false);
+//
+//        gradeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                CommonCode subject = (CommonCode) subjectSpinner.getSelectedItem();
+//                CommonCode fileType = (CommonCode) typeSpinner.getSelectedItem();
+//                loadMyTeachingAids(fileType.getCodeID(), language, grades.get(i).getCodeID() , subject.getCodeID());
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
+//
+//        SpinnerArrayAdapter typeAdapter = new SpinnerArrayAdapter(getActivity(), R.layout.custom_spinner_item_header,
+//                types);
+//        typeAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+//        typeSpinner.setAdapter(typeAdapter);
+//        typeSpinner.setSelection(0 ,false);
+//
+//        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                CommonCode subject = (CommonCode) subjectSpinner.getSelectedItem();
+//                CommonCode grade = (CommonCode) gradeSpinner.getSelectedItem();
+//                loadMyTeachingAids(types.get(i).getCodeID(), language, grade.getCodeID() , subject.getCodeID());
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
 
         loadMyTeachingAids("", language, "", "");
 
