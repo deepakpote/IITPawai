4eafb268df4b03c4eb72371b68257cd1fb914d95
Pradnya Baviskar <pradnya@soft-corner.com>
2017-01-27 18:50:47 +0530

    Matched the UI design to that sent by Meghana

--- webPortal/js/mitra.webPortal.js
+++ webPortal/js/mitra.webPortal.js
@@ -1,7 +1,7 @@
-var webPortal = angular.module("webPortal", ['ngRoute','ngCookies','ngMessages'])
-				.config(config)
-				.run(run);
-
+var webPortal = angular.module("webPortal", ['ngCookies','ngMessages','ui.router','ui.bootstrap'])
+/*				.config(config)
+				.run(run)*/;
+/*
 config.$inject = ['$routeProvider', '$locationProvider'];
 function config($routeProvider, $locationProvider) {
     $routeProvider.
@@ -14,8 +14,73 @@ function config($routeProvider, $locationProvider) {
      otherwise({
         redirectTo: '/login'
      });
-}
+}*/
 
+console.log("reached here...");
+webPortal.
+config(['$stateProvider', '$urlRouterProvider',
+  function config($stateProvider, $urlRouterProvider) {
+	//$urlRouterProvider.when("", "/home");
+	$urlRouterProvider.otherwise('/home');
+
+	console.log("Entered config");
+	$stateProvider
+	/*.state('home', {
+        url: '/home',
+        templateUrl: 'home/homeLayout.html'
+    })*/
+    // HOME STATES AND NESTED VIEWS
+	.state('main', {
+        abstract: true,
+        url: '',
+        templateUrl: 'common/mitraLayoutView.html'
+      })
+      .state('main.index', {
+    	abstract: true,
+        url: '',
+        views: {
+        	'header': {
+        		templateUrl: 'common/headerView.html'
+        	},
+        	'leftMenu': {
+        		templateUrl: 'common/leftMenuView.html'
+        	},
+        	'contentBox': {
+        		templateUrl: 'common/contentBoxView.html'
+        	},
+        },
+    })
+	.state('main.index.home', {
+		url: '/home',
+		views: {
+			'content': {
+				templateUrl: 'home/layoutView.html'
+				//template: "Setting up home layout"
+			},
+        	'welcome@main.index.home': {
+        		templateUrl: 'home/welcomeView.html',
+        		//template: 'Welcome!',
+                controller: function($scope) {
+                    $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                }
+        	},
+        	
+        	'map@main.index.home': {
+        		templateUrl: 'home/mapView.html',
+                controller: function($scope) {
+                    console.log('hit map controller');
+                }
+        	}
+        }
+    });
+  }
+]);
+
+angular.module('webPortal').run(
+		  ['$templateCache', function($templateCache){
+		    $templateCache.put('node_modules/angular-ui-bootstrap/template/accordion/accordion-group.html', undefined);
+		  }
+		]);
 
 run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
 function run($rootScope, $location, $cookies, $http) {
