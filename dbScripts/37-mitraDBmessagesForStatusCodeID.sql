b8d3bab1bdd0e72550331508bec2a8f2dda14dd4
Dipak Pote <deepakpote.007@gmail.com>
2017-02-22 14:40:02 +0530

    Comment changed for adding new error message

--- dbScripts/37-mitraDBmessagesForStatusCodeID.sql
+++ dbScripts/37-mitraDBmessagesForStatusCodeID.sql
@@ -1,4 +1,4 @@
--- Add messages for user role list .
+-- Add messages for search teaching AID & searchSelfLearning API.
 Insert into com_code(codeID, codeGroupID, codeNameEn, codeNameMr, createdBy, createdOn, modifiedBy, modifiedOn, comment)
 Select 100208 as codeID, 100 as codeGroupID, 'Status does not exists' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Search teaching aid/selff learning' as comment;
 
