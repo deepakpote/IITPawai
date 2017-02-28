c3a88e3eecc2aab5329821cb4efc2fcc6f42e7c4
Tanmayee Takalkar <tanmayee21193@gmail.com>
2017-02-28 17:18:00 +0530

    Added self learning videos on dashboard

--- webPortal/js/dashboard/dashboardController.js
+++ webPortal/js/dashboard/dashboardController.js
@@ -80,23 +80,83 @@ angular.module("mitraPortal").controller("dashboardController",
 			 }]		  
 	  };
 	  
+	  var responseSelfLearning = {
+			  "data": [
+				   {
+					  "contentID": 262,
+					  "contentTitle": "कोन",
+					  "contentType": "107100",
+					  "topic": null,
+					  "requirementCodeIDs": "117100,117101,117102,117103",
+					  "instruction": "",
+					  "fileType": "108100",
+					  "fileName": "https://youtu.be/TKKpihENPrA",
+					  "author": "thefreemath.org",
+					  "objectives": "कोन",
+					  "language": "101101"
+				  },
+				  {
+					  "contentID": 263,
+					  "contentTitle": "कोन",
+					  "contentType": "107100",
+					  "topic": null,
+					  "requirementCodeIDs": "117100,117101,117102,117103",
+					  "instruction": "",
+					  "fileType": "108100",
+					  "fileName": "http://www.youtube.com/watch?v=iwGFalTRHDA",
+					  "author": "thefreemath.org",
+					  "objectives": "कोन",
+					  "language": "101101"
+				 },
+				 {
+					  "contentID": 264,
+					  "contentTitle": "कोन",
+					  "contentType": "107100",
+					  "topic": null,
+					  "requirementCodeIDs": "117100,117101,117102,117103",
+					  "instruction": "",
+					  "fileType": "108100",
+					  "fileName": "https://youtu.be/TKKpihENPrA",
+					  "author": "thefreemath.org",
+					  "objectives": "कोन",
+					  "language": "101101"
+				 },
+				 {
+					  "contentID": 265,
+					  "contentTitle": "कोन",
+					  "contentType": "107100",
+					  "topic": null,
+					  "requirementCodeIDs": "117100,117101,117102,117103",
+					  "instruction": "",
+					  "fileType": "108100",
+					  "fileName": "https://youtu.be/TKKpihENPrA",
+					  "author": "thefreemath.org",
+					  "objectives": "कोन",
+					  "language": "101101"
+				 }]	  
+		  };
+	  
 	  $scope.myInterval = 0;
 	  $scope.myTransition = false;
 	  $scope.noWrapSlides = false;
 	  $scope.active = 0;
-	  var slides = $scope.slides = [];
-	  var currIndex = 0;
-	  var slideCollection =  $scope.slideCollection = [];
+	  var teachingAidsCollection =  $scope.teachingAidsCollection = [];
+	  var selfLearningCollection =  $scope.selfLearningCollection = [];
 
-	  $scope.makeCollectionOfThree = function() {
+	  $scope.makeCollectionOfThree = function(slides, type) {
 		  for (var i = 0; i < slides.length; i+=3) {
 			  var slideGroup = {
 					  	id: i/3,
-					  	first : $scope.slides[i] || {},
-			  			second: $scope.slides[i+1] || {},
-					  	third : $scope.slides[i+2] || {}
+					  	first : slides[i] || {},
+			  			second: slides[i+1] || {},
+					  	third : slides[i+2] || {}
 			  }
-			  slideCollection.push(slideGroup)
+			  if(type === 0) {
+				  teachingAidsCollection.push(slideGroup); 
+			  }
+			  else{
+				  selfLearningCollection.push(slideGroup)
+			  }
 		  }
 	  };
 	  
@@ -106,27 +166,36 @@ angular.module("mitraPortal").controller("dashboardController",
 		    return (match&&match[7].length==11)? match[7] : false;
 	  };
 	  
-	 $scope.addSlides = function() {
-			for(var i=0; i< responseTeachingAids.data.length; i++){
+	 $scope.add = function(slides, type) {
+		 response = (type === 0) ? responseTeachingAids : responseSelfLearning;
+		 
+			for(var i=0; i< response.data.length; i++){
 				slides.push({
-					videoURL: responseTeachingAids.data[i].fileName,
-					image: 'http://img.youtube.com/vi/' + $scope.getFileName(responseTeachingAids.data[i].fileName) + '/0.jpg',
-					contentID: responseTeachingAids.data[i].contentID
+					videoURL: response.data[i].fileName,
+					image: 'http://img.youtube.com/vi/' + $scope.getFileName(response.data[i].fileName) + '/0.jpg',
+					contentID: response.data[i].contentID
 				})
 			}
+			return slides;
 	};
 	
 	$scope.playVideo = function ( url ) {
 		 $window.location.href = url
-	}
+	};
 	
 	$scope.goTo = function ( state ) {
 		$state.go(state)
 	};
 	
   	var init = function () {
-  	  $scope.addSlides();
-	  $scope.makeCollectionOfThree()
+  	  var teachingAids = []; 
+  	  var selfLearningVideos = []; 
+  	  
+  	  teachingAids = $scope.add(teachingAids, 0);
+	  $scope.makeCollectionOfThree(teachingAids, 0);
+	  
+	  selfLearningVideos = $scope.add(selfLearningVideos, 1);
+	  $scope.makeCollectionOfThree(selfLearningVideos, 1)
   	}
   	
   	init();
