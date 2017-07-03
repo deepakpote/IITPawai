78f3f41e7f97f3e52f14d5b0b485e3bcd844ea56
Dipak Pote <deepakpote.007@gmail.com>
2017-07-03 17:09:11 +0530

    Send notification bug fix

--- webPortal/js/notifications/mitra.notificationsController.js
+++ webPortal/js/notifications/mitra.notificationsController.js
@@ -1,14 +1,15 @@
 angular.module("mitraPortal").controller("mitra.notificationsController", function($scope,$rootScope, NotificationsService) {
 	$scope.sendDataNotifications = function(){
 		dataMessage = $scope.notificationDataMessage;
-		alert("Token: " + $rootScope.globals.currentUser.token);
-		NotificationsService.sendDataNotifications(dataMessage, $rootScope.globals.currentUser.token)
+		//alert("Token: " + $rootScope.globals.currentUser.token);
+		NotificationsService.sendDataNotifications(dataMessage)
 				.then(
 					function(response){
-						alert(response.data.response_message);
+						alert("Notification sent successfully");
 					}, 
 					function(response){
-						alert("error");
+						alert("There was an error sending notification.")
+						console.log(response);
 					}
 				);
 		
