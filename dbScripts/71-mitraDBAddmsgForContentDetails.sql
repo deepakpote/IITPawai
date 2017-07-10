efbbcfd503d10dcf647883f119adee5ee3b940ae
Dipak Pote <deepakpote.007@gmail.com>
2017-07-10 13:45:54 +0530

    Added correct SQL file

--- dbScripts/71-mitraDBAddmsgForContentDetails.sql
+++ dbScripts/71-mitraDBAddmsgForContentDetails.sql
@@ -1,14 +1,11 @@
 -- Add messages for get content details for specific all language.
 Insert into com_code (codeID, codeGroupID, codeNameEn, codeNameMr, createdBy, createdOn, modifiedBy, modifiedOn, comment)
-Select 100288 as codeID, 100 as codeGroupID, 'Invalid token' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Send data notification to all' as comment union
-Select 100289 as codeID, 100 as codeGroupID, 'NotificationTypeCodeID cannot be empty' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Send data notification to all' as comment union
-Select 100290 as codeID, 100 as codeGroupID, 'ObjectID cannot be empty' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Send data notification to all' as comment union
-Select 100291 as codeID, 100 as codeGroupID, 'Marathi title cannot be empty' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Send data notification to all' as comment union
-Select 100292 as codeID, 100 as codeGroupID, 'Marathi text cannot be empty' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Send data notification to all' as comment union
-Select 100293 as codeID, 100 as codeGroupID, 'No FCM deviceID found' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , 'Send data notification to all' as comment;
+Select 100294 as codeID, 100 as codeGroupID, 'App language cannot be empty' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , ' get content details for specific all language' as comment union
+Select 100295 as codeID, 100 as codeGroupID, 'App language not exists' as codeNameEn, '' as codeNameMr, 1 as createdBy, now() as createdOn,  1 as modifiedBy, now() as modifiedOn , ' get content details for specific all language' as comment;
 
 -- update com code version.
 update com_configuration set value = value + 1 where `key` = 'comCodeVersion';
 
 -- update into com_configuration
 update com_configuration set `value` = '71-mitraDBAddmsgForContentDetails' where `key` = 'dbScriptExecutedUntil';
+
